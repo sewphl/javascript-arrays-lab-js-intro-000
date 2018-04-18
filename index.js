5e0bd8c92a6eba67b241999d2f90d2af7ce167a1
sharon <sharonoptsout@gmail.com>
2018-04-18 20:47:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const app = "I don't do much."
+
+//var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name) {
+  var kittens = ["Milo", "Otis", "Garfield"];
+  kittens.push(name);
+  //return(kittens);
+}
